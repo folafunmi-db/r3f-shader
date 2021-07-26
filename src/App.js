1938de8fac3437238db136e6c9f655b0d602fb3d
Folafunmi <folafunmimustapha@gmail.com>
2021-07-26 10:13:13 +0100

    feat: try to add shader😂

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, extend } from "@react-three/fiber";
+import { shaderMaterial } from "@react-three/drei";
+import glsl from "babel-plugin-glsl/macro";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const WaveShaderMaterial = shaderMaterial(
+	// Uniform
+	{},
+	// Vertex Shader
+	glsl`
+    void main() {
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(postion, 1.0);
+    }
+`,
+	// Fragment Shader
+	glsl`
+    void main() {
+      gl_FragColor = vec4(0.0, 0.4, 1.0, 1.0);
+    }
+  `
+);
+
+extend({ WaveShaderMaterial });
+
+const Scene = () => {
+	return (
+		<Canvas>
+			<pointLight position={[10, 10, 10]} />
+			<mesh>
+				<planeBufferGeometry args={[3, 5]} />
+				<waveShaderMaterial />
+			</mesh>
+		</Canvas>
+	);
+};
+
+const App = () => {
+	return <Scene />;
+};
 
 export default App;
